e66b01078094c36ce343179e35244f640d130c17
Deepak Singh <deepakksingh1202@gmail.com>
2026-03-28 23:05:52 +0530

    refactor(client): provide redux store at app root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
+import { store } from './app/store.js'
 import { AppRouter } from './router/AppRouter.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Toaster position="top-right" />
-    <AppRouter />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <>
+      <Toaster position="top-right" />
+      <AppRouter />
+    </>
+  </Provider>,
 )
