210c1e23727a4db5334234fbd5726167d82695e2
Deepak Singh <deepakksingh1202@gmail.com>
2026-04-07 00:56:31 +0530

    chore: remove legacy axios client and protected route logic replaced by RTK Query

--- client/src/components/ProtectedRoute.jsx
+++
@@ -1,3 +0,0 @@
-export { default as ProtectedRoute } from '../router/ProtectedRoute';
-export { default as AdminRoute } from '../router/AdminRoute';
-export { default as GuestRoute } from '../router/GuestRoute';
