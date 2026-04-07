3cb2af4daa9fcf95741891df89bf9a430f498819
Deepak Singh <deepakksingh1202@gmail.com>
2026-04-07 12:29:39 +0530

    feat: initialize Redux Toolkit store and configure base RTK Query apiSlice

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,20 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer         from './slices/authSlice';
-import scoreReducer        from './slices/scoreSlice';
-import charityReducer      from './slices/charitySlice';
-import drawReducer         from './slices/drawSlice';
-import subscriptionReducer from './slices/subscriptionSlice';
-import adminReducer        from './slices/adminSlice';
 import { apiSlice }        from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    auth:         authReducer,
-    scores:       scoreReducer,
-    charities:    charityReducer,
-    draws:        drawReducer,
-    subscription: subscriptionReducer,
-    admin:        adminReducer,
+    auth:                   authReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefault) =>
