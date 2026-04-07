322c86ad0d40d62bd5f7f2757fc447b90a10c090
Deepak Singh <deepakksingh1202@gmail.com>
2026-04-07 16:26:08 +0530

    Fix ESLint compatibility for Vercel builds

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import react from 'eslint-plugin-react'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -13,6 +14,9 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      react,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -24,6 +28,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-uses-vars': 'error',
+      'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': 'off',
     },
   },
